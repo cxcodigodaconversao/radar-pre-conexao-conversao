541dfb3b6ac5612bd53f87ad52dab7aa7500c16c
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-08-08 12:45:15 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RadarPreConexao from './components/RadarPreConexao';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RadarPreConexao />
+    </div>
+  );
+}
+
+export default App;
